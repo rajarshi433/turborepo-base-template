367a46ed0e0c1bd1718f35f2cd77076dac614542
rajarshi433 <mannarajarshi65@gmail.com>
2024-09-13 23:42:40 +0530

    fixed some bugs

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,13 +1,14 @@
-import { VAlUE } from "@repo/common/user-type";
+import { name, new_type, VAlUE } from "@repo/common/user-type";
 
 import express from "express";
 
 const app = express();
 
 app.get("/", (req, res) => {
+  console.log("Hello from backend")
   res.json({
-    message: `Hello ${VAlUE}`,
+    message: `Hello ${VAlUE} and  I am from ${name} and this a ${new_type}`,
   });
 });
 
-app.listen(3000);
+app.listen(5000);
--- packages/common/src/user-type.ts
+++ packages/common/src/user-type.ts
@@ -1,1 +1,5 @@
 export const VAlUE = "Rajarshi Manna"
+
+export const name = "Hello this is from common file"
+
+export const new_type = "new Type"
